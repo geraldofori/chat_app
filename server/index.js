d4d871f05177b2f5c29291d4ba9eb9718488d505
gerald <oforige@gmail.com>
2022-11-05 11:16:03 +0000

    added a gitignore

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+
+const httpServer = require('http').createServer(app);
+
+const io = require('socket.io')(httpServer,{
+    cors: { origin: '*'}
+});
+
+const port = process.env.PORT || 3000;
+
+
+
+io.on('connection', (socket)=> {
+    console.log('User is connected!');
+
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+      });
+
+    socket.on('disconnect', () => {
+        console.log('User is disconnected!');
+      });
+
+});
+
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
