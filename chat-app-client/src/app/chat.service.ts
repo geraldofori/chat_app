8e36adcd6c87971bd62264d1f0a712d69de6a02a
gerald <oforige@gmail.com>
2022-11-06 12:02:33 +0000

    added material ui to project

--- chat-app-client/src/app/chat.service.ts
+++ chat-app-client/src/app/chat.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {io, Socket} from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private socket:Socket;
+  private url = 'https://localhost:3000';
+
+
+  constructor() {
+    this.socket = io(this.url);
+  }
+
+  joinChatRoom(data): void{
+    this.socket.emit('join',data);
+
+  }
+
+  sendMessage(data): void{
+    this.socket.emit('message',data);
+  }
 }
