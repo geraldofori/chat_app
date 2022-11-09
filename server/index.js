654fa965216f23d2e6b7e4d76c071ea63cedcb40
gerald <oforige@gmail.com>
2022-11-09 04:42:19 +0000

    restructuring the server and install cors

--- server/index.js
+++ server/index.js
@@ -1,35 +1,91 @@
+// const express = require('express');
+//
+// const app = express();
+//
+// const http = require('http');
+//
+// const cors = require('cors');
+//
+// const server = http.createServer(app);
+//
+//
+// const {Server} = require('socket.io');
+// const io = new Server(server, {
+//     cors: {
+//         origin: '*',
+//         methods: ['GET', 'POST']
+//     },
+// });
+//
+// const port = process.env.PORT || 3000;
+//
+// server.listen(port, ()=>{
+//     console.log(`Listening on port : ${port}`);
+// })
+//
+// io.on('connection', (socket)=> {
+//
+//     socket.on('message', (msg) => {
+//         // socket.join(data.room);
+//         socket.broadcast.emit('message',{user: msg.user, message: msg.message});
+//       });
+//
+//     socket.on('disconnect', () => {
+//         console.log('User is disconnected!');
+//       });
+//
+// });
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+const http = require('http').createServer(app);
 
-const http = require('http');
-
-const server = http.createServer(app);
-
-
-const {Server} = require('socket.io');
-const io = new Server(server, {
+const io = require('socket.io')(http, {
     cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    },
+        origin: '*'
+    }
 });
 
-const port = process.env.PORT || 3000;
+// app.get('/', (req, res) => {
+//     res.send('Heello world');
+// })
 
-server.listen(port, ()=>{
-    console.log(`Listening on port : ${port}`);
-})
+let userList = new Map();
 
-io.on('connection', (socket)=> {
+io.on('connection', (socket) => {
+    let userName = socket.handshake.query.userName;
+    addUser(userName, socket.id);
+
+    socket.broadcast.emit('user-list', [...userList.keys()]);
+    socket.emit('user-list', [...userList.keys()]);
 
     socket.on('message', (msg) => {
-        // socket.join(data.room);
-        socket.broadcast.emit('message',{user: msg.user, message: msg.message});
-      });
+        socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
+    })
 
-    socket.on('disconnect', () => {
-        console.log('User is disconnected!');
-      });
+    socket.on('disconnect', (reason) => {
+        removeUser(userName, socket.id);
+    })
+});
 
+function addUser(userName, id) {
+    if (!userList.has(userName)) {
+        userList.set(userName, new Set(id));
+    } else {
+        userList.get(userName).add(id);
+    }
+}
+
+function removeUser(userName, id) {
+    if (userList.has(userName)) {
+        let userIds = userList.get(userName);
+        if (userIds.size == 0) {
+            userList.delete(userName);
+        }
+    }
+}
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running ${process.env.PORT || 3000}`);
 });
