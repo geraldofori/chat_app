3ec0ea424fcbd4a6dec97bff5e997f28cb1a0916
gerald <oforige@gmail.com>
2022-11-10 14:48:13 +0000

    new chat app structure using bootstrap

--- chat-app-client/src/app/app-routing.module.ts
+++ chat-app-client/src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import {LoginComponent} from "./components/login/login.component";
 import {ChatComponent} from "./components/chat/chat.component";
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'chat',
-    component: ChatComponent
-  },
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  // {
+  //   path: 'login',
+  //   component: LoginComponent
+  // },
+  // {
+  //   path: 'chat',
+  //   component: ChatComponent
+  // },
+  // {path: '', redirectTo: '', pathMatch: 'full'},
 ];
 
 @NgModule({
